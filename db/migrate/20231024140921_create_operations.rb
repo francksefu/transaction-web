ad6424662183ae96adaf9e6a0fefdb4edf5c860d
francksefu <francksefu1998@gmail.com>
2023-10-24 17:39:09 +0200

    set up project

---
+++ db/migrate/20231024140921_create_operations.rb
@@ -0,0 +1,11 @@
+class CreateOperations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :operations do |t|
+      t.string :name
+      t.integer :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
